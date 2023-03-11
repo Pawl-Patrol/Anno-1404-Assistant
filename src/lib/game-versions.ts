faf7b3ccafbd796955d93ca536d89926452340b2
Paul <pawl-patrol@protonmail.ch>
2023-03-11 11:38:57 +0100

    fix offsets

--- src/lib/game-versions.ts
+++ src/lib/game-versions.ts
@@ -10,6 +10,6 @@ export const gameVersions: GameVersion[] = [
   {
     name: "Anno 1404 Venice (History Edition)",
     version: "v4.00.4253/1.5231.951665",
-    pointerPath: [0x02099b08n, 0x8c4n],
+    pointerPath: [0x02099b08n, 0x8e4n],
   },
 ];
--- src/lib/population-offsets.ts
+++ src/lib/population-offsets.ts
@@ -1,11 +1,11 @@
 import { Population } from "./population";
 
 export const populationOffsets = {
-  Beggars: 32n,
-  Nomads: 96n,
-  Envoys: 128n,
-  Peasants: 192n,
-  Citizens: 224n,
-  Patricians: 256n,
-  Noblemen: 288n,
+  Beggars: 0n,
+  Nomads: 64n,
+  Envoys: 96n,
+  Peasants: 160n,
+  Citizens: 192n,
+  Patricians: 224n,
+  Noblemen: 256n,
 } satisfies Record<Population, bigint>;
